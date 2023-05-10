84f6e5efc433c21f5b68c14b9bd4b2968b6b5038
kirill <lobbi.kirill@mail.ru>
2023-05-10 16:24:16 +0300

    Написали функцию format_score(score_str: str) -> str: что бы стринг преобразовывать в нужном формате: •	Номер карты замаскирован и не отображается целиком в формате XXXX XX** **** XXXX (видны первые 6 цифр и последние 4, разбито по блокам по 4 цифры, разделенных пробелом). •	Номер счета замаскирован и не отображается целиком в формате **XXXX (видны только последние 4 цифры номера счета).

--- utils.py
+++ utils.py
@@ -24,3 +24,34 @@ def last_executed(data: list, number) -> list:
     data.sort(key=lambda element: element["date"], reverse=True)
     temp_data = data[:number]
     return temp_data
+
+def formatted_data(data: list) -> list:
+    return_data=[]
+    for item in data:
+        str1 = item["date"] + ' ' + item["description"]
+
+        if 'from' in item:
+            # str2 = " " + ' -> ' + str(item["to"]).split()[0] + ' ' + str(item["to"]).split()[1]
+            sender = item['to']
+            str2 = f" -> {format_score(sender)}"
+        else:
+            str2 = ' -> ' + ''
+        str3 = item["operationAmount"]["amount"] + ' ' + item["operationAmount"]["currency"]["name"]
+        print(str1)
+        print(str2)
+        print(str3)
+        print(' ')
+
+
+    return return_data
+
+
+def format_score(score_str: str) -> str:
+    temp_str = score_str.split()
+    if temp_str[0] == 'Счет':
+        return f'{temp_str[0]} **{temp_str[1][-5:-1]}'
+    else:
+        return f'{temp_str[0]} {temp_str[1][:4]} {temp_str[1][4:6]}** **** {temp_str[1][12:16]}'
+
+
+
