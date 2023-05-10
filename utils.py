3fd1892a239e83b43ba4d549c9a1182db23903eb
kirill <lobbi.kirill@mail.ru>
2023-05-10 16:57:48 +0300

    Написали функцию formatted_data(data: list) -> list: что бы финальные данные преобразовывать в нужный формат: 14.10.2018 Перевод организации Visa Platinum 7000 79** **** 6361 -> Счет **9638 82771.72 руб.

--- utils.py
+++ utils.py
@@ -28,21 +28,22 @@ def last_executed(data: list, number) -> list:
 def formatted_data(data: list) -> list:
     return_data=[]
     for item in data:
-        str1 = item["date"] + ' ' + item["description"]
+        date = f'{item["date"][8:10]}.{item["date"][5:7]}.{item["date"][:4]}'
+        str1 = date + ' ' + item["description"]
 
         if 'from' in item:
-            # str2 = " " + ' -> ' + str(item["to"]).split()[0] + ' ' + str(item["to"]).split()[1]
-            sender = item['to']
-            str2 = f" -> {format_score(sender)}"
+            sender = item["from"]
+            recipient = item["to"]
+            str2 = f"{format_score(sender)} -> {format_score(recipient)}"
         else:
-            str2 = ' -> ' + ''
+            recipient = item["to"]
+            str2 = f"-> {format_score(recipient)}"
         str3 = item["operationAmount"]["amount"] + ' ' + item["operationAmount"]["currency"]["name"]
-        print(str1)
-        print(str2)
-        print(str3)
-        print(' ')
-
-
+        return_data.append(f"""\
+{str1}
+{str2}
+{str3}
+""")
     return return_data
 
 
@@ -51,7 +52,10 @@ def format_score(score_str: str) -> str:
     if temp_str[0] == 'Счет':
         return f'{temp_str[0]} **{temp_str[1][-5:-1]}'
     else:
-        return f'{temp_str[0]} {temp_str[1][:4]} {temp_str[1][4:6]}** **** {temp_str[1][12:16]}'
+        if len(temp_str) == 2:
+            return f'{temp_str[0]} {temp_str[1][:4]} {temp_str[1][4:6]}** **** {temp_str[1][12:16]}'
+        if len(temp_str) == 3:
+            return f'{temp_str[0]} {temp_str[1]} {temp_str[2][:4]} {temp_str[2][4:6]}** **** {temp_str[2][12:16]}'
 
 
 
