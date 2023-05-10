75570c19564ed293faf994f4598419011c0e9ff7
kirill <lobbi.kirill@mail.ru>
2023-05-10 21:39:07 +0300

    Добавлено тестирование def test_format_score(): для функции format_score

--- Tests/test_utils.py
+++ Tests/test_utils.py
@@ -3,7 +3,13 @@ from utils import *
 
 
 def test_load_data():
-    data = load_data(os.path.join('operations.json'))
+    data = load_data(os.path.join('./data/operations.json'))
     assert isinstance(data, list)
     assert load_data(os.path.join(' ')) == None
+
+
+def test_format_score():
+    assert format_score("Счет 90424923579946435907") == 'Счет **3590'
+    assert format_score("Visa Classic 2842878893689012") == 'Visa Classic 2842 87** **** 9012'
+    assert format_score("Maestro 7810846596785568") == 'Maestro 7810 84** **** 5568'
 
